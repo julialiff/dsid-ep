01ab40627a588a639cf056d409489e657ea61ef1
Julia Litvinoff Justus <julialiff@hotmail.com>
2018-06-23 22:26:29 -0300

    Added log

--- greeter_client.py
+++ greeter_client.py
@@ -19,30 +19,52 @@ import grpc
 
 import projeto_DSID_pb2
 import projeto_DSID_pb2_grpc
+import logging
+import time
 
+LOG_FILENAME = time.strftime('%Y-%m-%d', time.localtime(time.time()))+'.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 
 def run():
-	channel = grpc.insecure_channel('localhost:50051')
-	stub = projeto_DSID_pb2_grpc.GreeterStub(channel)
-	# response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
-	# print("Greeter client received: " + response.message)
-	# response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
-	# print("Greeter client received: " + response.message)
-	
-	response = stub.ChamadaVoid(projeto_DSID_pb2.RequestLong())
-	print("Greeter client received from void call: ")
+  # remote call
+  # channel = grpc.insecure_channel('13.59.82.197:50051')
 
-	response = stub.ChamadaLong(projeto_DSID_pb2.RequestLong(num=99999999999))
-	print("Greeter client received from long call: " + str(response.message))
-	
-	response = stub.Chamada8Long(projeto_DSID_pb2.Request8Long( num1=99999999999,num2=99999999999,num3=99999999999,num4=99999999999,num5=99999999999,num6=99999999999,num7=99999999999,num8=99999999999, ))
-	print("Greeter client received from 8 long call: " + str(response.message))
-	
-	for n in range(11):
-		response = stub.ChamadaString(projeto_DSID_pb2.RequestString(name="a"*2**n))
-		print("Greeter client received from string call: " + str(response.message))
-		
-	
+  # local call
+  channel = grpc.insecure_channel('localhost:50051')
+
+  stub = projeto_DSID_pb2_grpc.GreeterStub(channel)
+  # response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+  # print("Greeter client received: " + response.message)
+  # response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
+  # print("Greeter client received: " + response.message)
+  time_initial = time.time()
+  response = stub.ChamadaVoid(projeto_DSID_pb2.RequestLong())
+  time_final = time.time()
+  total = time_final - time_initial
+  logging.debug("Void call time: %f" % total)
+  print("Client received from void call: ")
+
+  time_initial = time.time()
+  response = stub.ChamadaLong(projeto_DSID_pb2.RequestLong(num=99999999999))
+  time_final = time.time()
+  total = time_final - time_initial
+  logging.debug("Long call time: %f" % total)
+  print("Client received from long call: " + str(response.message))
+
+  time_initial = time.time()
+  response = stub.Chamada8Long(projeto_DSID_pb2.Request8Long( num1=99999999999,num2=99999999999,num3=99999999999,num4=99999999999,num5=99999999999,num6=99999999999,num7=99999999999,num8=99999999999, ))
+  time_final = time.time()
+  total = time_final - time_initial
+  logging.debug("8 Long call time: %f" % total)
+  print("Client received from 8 long call: " + str(response.message))
+
+  for n in range(11):
+    time_initial = time.time()
+    response = stub.ChamadaString(projeto_DSID_pb2.RequestString(name="a"*2**n))
+    time_final = time.time()
+    total = time_final - time_initial
+    logging.debug("String call time #%d: %f" % (n, total))
+    print("Client received from string call: " + str(response.message))
 
 if __name__ == '__main__':
     run()
